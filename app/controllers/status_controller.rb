a546ed60dabf4472b8e994536822b6247f5a8c76
Rob Kaufman <rob@notch8.com>
2025-01-28 14:27:02 -0800

    i18n

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -18,10 +18,10 @@ class StatusController < ApplicationController
     result = if @endpoint.restart_command.present? && @endpoint.last_restart < 5.minutes.ago
                `#{@endpoint.restart_command}`
              else
-               'no run'
+               t('.not_run')
              end
     @endpoint.update(last_restart: Time.zone.now)
-    flash[:notice] = "Restart result was: #{result}"
+    flash[:notice] = t('.restart_result', result: result)
     redirect_to status_path
   end
 end
