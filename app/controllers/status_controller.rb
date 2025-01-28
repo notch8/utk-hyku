5f21920bba3b5bc46be22bc657c7364f30030fb5
Rob Kaufman <rob@notch8.com>
2025-01-28 14:27:53 -0800

    button to restart upstream services from status menu (#689)
    * button to restart upstream services from status menu

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -7,9 +7,21 @@ class StatusController < ApplicationController
     authorize! :read, :admin_dashboard
   end
 
-  def index
+  def show
     add_breadcrumb t(:'hyrax.controls.home'), root_path
     add_breadcrumb t(:'hyrax.dashboard.breadcrumbs.admin'), hyrax.dashboard_path
     add_breadcrumb t(:'hyrax.admin.sidebar.system_status'), main_app.status_path
+  end
+
+  def update
+    @endpoint = Endpoint.find(params[:id])
+    result = if @endpoint.restart_command.present? && @endpoint.last_restart < 5.minutes.ago
+               `#{@endpoint.restart_command}`
+             else
+               t('.not_run')
+             end
+    @endpoint.update(last_restart: Time.zone.now)
+    flash[:notice] = t('.restart_result', result: result)
+    redirect_to status_path
   end
 end
