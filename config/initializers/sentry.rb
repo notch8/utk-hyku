6283cf2bb236f09106edac3a1d0b8dfe5390dba0
April Rieger <april.rieger@yahoo.com>
2025-06-05 19:41:13 -0700

    Setup sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'sentry-ruby'
+require 'sentry-sidekiq'
+
+Sentry.init do |config|
+  config.dsn = ENV['SENTRY_DSN']
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.enabled_environments = %w[utk-hyku-friends utk-hyku-production]
+  config.debug = true
+end
