3efd28d01a5fa751d391838a09321d4fd9cb2a65
Max Kadel <max@notch8.com>
2026-01-28 18:16:16 +0100

    Fix syntax

--- app/jobs/cleanup_sub_directory_job.rb
+++ app/jobs/cleanup_sub_directory_job.rb
@@ -20,10 +20,10 @@ class CleanupSubDirectoryJob < ApplicationJob
     end
 
     def should_be_deleted?(path)
-      File.file?(path) && ((old_enough(path) && fileset_created?(path)) || very_old?(path))
+      File.file?(path) && ((old_enough?(path) && fileset_created?(path)) || very_old?(path))
     end
 
-    def old_enough(path)
+    def old_enough?(path)
       return true if File.mtime(path) < (Time.zone.now - days_old.to_i.days)
 
       false
