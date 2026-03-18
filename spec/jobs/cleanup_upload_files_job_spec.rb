67ba8bcd1692bbe235cec8d80491f57d6ac5eddf
April Rieger <april.rieger@yahoo.com>
2026-03-18 14:41:40 -0700

    Add specs for protected upload dirs in cleanup job (#842)Document that CleanupUploadFilesJob never runs for tenant UUID dirs,uploaded_collection_thumbnails, identity_provider, or hyrax (UploadedFilecache). Only hex pair-tree dirs (00-ff) are cleaned

--- spec/jobs/cleanup_upload_files_job_spec.rb
+++ spec/jobs/cleanup_upload_files_job_spec.rb
@@ -5,28 +5,59 @@ RSpec.describe CleanupUploadFilesJob do
   let(:hex_dir_00) { '/app/samvera/uploads/00' }
   let(:hex_dir_ab) { '/app/samvera/uploads/ab' }
   let(:uuid_tenant_dir) { '/app/samvera/uploads/56e0eb81-c2d5-4d5d-9171-b251bf7299a4' }
+  let(:uploaded_collection_thumbnails_dir) { '/app/samvera/uploads/uploaded_collection_thumbnails' }
+  let(:identity_provider_dir) { '/app/samvera/uploads/identity_provider' }
+  let(:hyrax_uploaded_file_dir) { '/app/samvera/uploads/hyrax' }
+
+  let(:all_top_level_entries) do
+    [
+      hex_dir_ff, hex_dir_00, hex_dir_ab,
+      uuid_tenant_dir,
+      uploaded_collection_thumbnails_dir,
+      identity_provider_dir,
+      hyrax_uploaded_file_dir,
+      '/app/samvera/uploads/somefile'
+    ]
+  end
 
   before do
     allow(Dir).to receive(:glob).and_call_original
-    allow(Dir).to receive(:glob).with('/app/samvera/uploads/*')
-      .and_return([hex_dir_ff, hex_dir_00, hex_dir_ab, uuid_tenant_dir, '/app/samvera/uploads/somefile'])
+    allow(Dir).to receive(:glob).with('/app/samvera/uploads/*').and_return(all_top_level_entries)
     allow(File).to receive(:directory?).and_call_original
-    allow(File).to receive(:directory?).with(hex_dir_ff).and_return(true)
-    allow(File).to receive(:directory?).with(hex_dir_00).and_return(true)
-    allow(File).to receive(:directory?).with(hex_dir_ab).and_return(true)
-    allow(File).to receive(:directory?).with(uuid_tenant_dir).and_return(true)
+    [hex_dir_ff, hex_dir_00, hex_dir_ab, uuid_tenant_dir,
+     uploaded_collection_thumbnails_dir, identity_provider_dir, hyrax_uploaded_file_dir].each do |dir|
+      allow(File).to receive(:directory?).with(dir).and_return(true)
+    end
     allow(File).to receive(:directory?).with('/app/samvera/uploads/somefile').and_return(false)
   end
 
-  it 'spawns child jobs only for hex pair-tree directories (not tenant UUID dirs)' do
+  it 'spawns child jobs only for hex pair-tree directories (00-ff)' do
     expect { described_class.perform_now(delete_ingested_after_days: 180, uploads_path: '/app/samvera/uploads') }
       .to have_enqueued_job(CleanupSubDirectoryJob).exactly(3).times
   end
 
-  it 'does not create CleanupSubDirectoryJob for tenant UUID directories' do
+  it 'does not create CleanupSubDirectoryJob for tenant UUID directories (site/banner_images, etc.)' do
     expect do
       described_class.perform_now(delete_ingested_after_days: 180, uploads_path: '/app/samvera/uploads')
     end.not_to have_enqueued_job(CleanupSubDirectoryJob).with(directory: uuid_tenant_dir)
+  end
+
+  it 'does not create CleanupSubDirectoryJob for uploaded_collection_thumbnails directory' do
+    expect do
+      described_class.perform_now(delete_ingested_after_days: 180, uploads_path: '/app/samvera/uploads')
+    end.not_to have_enqueued_job(CleanupSubDirectoryJob).with(directory: uploaded_collection_thumbnails_dir)
+  end
+
+  it 'does not create CleanupSubDirectoryJob for identity_provider directory (LogoUploader)' do
+    expect do
+      described_class.perform_now(delete_ingested_after_days: 180, uploads_path: '/app/samvera/uploads')
+    end.not_to have_enqueued_job(CleanupSubDirectoryJob).with(directory: identity_provider_dir)
+  end
+
+  it 'does not create CleanupSubDirectoryJob for hyrax directory (UploadedFile cache)' do
+    expect do
+      described_class.perform_now(delete_ingested_after_days: 180, uploads_path: '/app/samvera/uploads')
+    end.not_to have_enqueued_job(CleanupSubDirectoryJob).with(directory: hyrax_uploaded_file_dir)
   end
 
   it 'passes delete_all_after_days and uploads_path to child jobs' do
